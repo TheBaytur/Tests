b88be31ad588618f3c9de8e856d1d0c043e68137
TheBaytur <145964486+TheBaytur@users.noreply.github.com>
2023-10-02 20:17:51 +0800

    Logic-if/else/return
    Bar logic

--- Main.java
+++ Main.java
@@ -1,35 +1,19 @@
-package com.company;
-
-import com.company.db.DB;
-
 public class Main {
-
     public static void main(String[] args) {
-
-        final int count = 10;
-        
-
-        Person obj1 = new Person();
-        Person obj2 = new Person();
-        Person obj3 = new Person();
-        obj3.getCount();
-        Person.getCount();
-
-//        Person.count = 5;
-//        System.out.println(Person.count);
-
-	DB db = new DB();
-
-	info();
-    }
-
-    public static void info() {
-        System.out.println("Hello");
+        print (drink(true,true));
+        print (drink(true,false));
+        print (drink(false,true));
+        print (drink(false,false));
 
     }
 
-    public static void info(String word) {
-        System.out.println(word + "!");
+    private static boolean drink(boolean isFriday, boolean haveMoney){
+        boolean goToDrink = false;
 
+        return isFriday || haveMoney;
+    }
+
+    private static void print(boolean result){
+        System.out.println(result);
     }
 }
