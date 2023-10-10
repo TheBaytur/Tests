f1d5f0621637c19278e21748f6e66711ab9cdc98
TheBaytur <145964486+TheBaytur@users.noreply.github.com>
2023-10-10 20:20:20 +0800

    -2
    Decreasing by 2

---
+++ Decrease.java
@@ -0,0 +1,11 @@
+public class Decrease {
+
+    public static void main(String[] args) {
+        String result = "";
+        for (int i = 20; i > 0; i -=2){
+            result += i;
+            result += " ";
+        }
+        System.out.println(result);
+    }
+}
