ff73bac7944e2d1112e5a81570b4696170832326
TheBaytur <145964486+TheBaytur@users.noreply.github.com>
2023-10-13 19:41:00 +0800

    Fibonacci starting with 2

--- Fibonacci.java
+++ Fibonacci.java
@@ -8,15 +8,13 @@ public class Fibonacci {
     public static void Fibonacci(int max){
         int item0 = 1;
         int item1 = 1;
-        for (int i = 0; i < max; i++){
-            if (i == 0 || i == 1) {
-                System.out.println(1);
-            } else {
-                System.out.println(item0 + item1);
-                int item10ld = item1;
-                item1 = item0 + item1;
-                item0 = item10ld;
+        for (int i = 2; i < max; i++){
+            System.out.println(item0 + item1);
+            int item10ld = item1;
+            item1 = item0 + item1;
+            item0 = item10ld;
+
             }
         }
-    }
+    
 }
