4c5c8a924267e9c576357f8496ddfe58e1725354
TheBaytur <145964486+TheBaytur@users.noreply.github.com>
2023-10-16 20:16:33 +0800

    Salary logic

---
+++ Salary.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+
+public class Salary {
+
+    private static final int CREDIT_MAX = 150000;
+    private static final int SALARY_MIN = 25000;
+    private static final int DURATION_MAX = 12;
+    private static final String DENIED = "Вам отказано в кредите";
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Добрый день, желаете взять в кредит? Какая у вам зарплата?");
+        int salary = scanner.nextInt();
+        if (salary < SALARY_MIN) {
+            System.out.println(DENIED);
+        } else {
+            System.out.println("Какой срок?");
+            int duration = scanner.nextInt();
+            if (duration > DURATION_MAX) {
+                System.out.println(DENIED);
+        } else {
+                System.out.println("Какая сумма?");
+                int credit = scanner.nextInt();
+                if (credit > CREDIT_MAX) {
+                    System.out.println(DENIED);
+                } else {
+                    System.out.println("Вам одобрен кредит на сумму " + CREDIT_MAX + "!");
+                }
+            }
+
+            }
+    }
+}
