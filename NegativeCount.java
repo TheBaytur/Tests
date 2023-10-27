cc1cfc1159f97c73d02f5e48dd27c861d85c3758
TheBaytur <145964486+TheBaytur@users.noreply.github.com>
2023-10-27 19:54:11 +0800

    Проверка на пустоту

---
+++ NegativeCount.java
@@ -0,0 +1,29 @@
+//import static java.lang.VersionProps.print;
+
+public class NegativeCount {
+    public static void main(String[] args) {
+        System.out.println(negativeCount(new int[]{-1, 3, 2, -2}));
+        System.out.println(negativeCount(new int[]{1, 3, 2, -2}));
+        System.out.println(negativeCount(new int[]{-1, -3, -2, -2}));
+        System.out.println(negativeCount(new int[]{}));
+
+    }
+
+    private static void print(int negativeCount) {
+    }
+
+    private static int negativeCount(int[] array) {
+        int count = 0;
+        if (array.length < 1){
+            System.out.println(("array is empty"));
+            count = -1;
+        } else {
+            for (int number : array) {
+                if (number < 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
