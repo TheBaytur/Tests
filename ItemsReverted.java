91173817fa0f012ddeef5ba9d76eb46be4afc419
TheBaytur <145964486+TheBaytur@users.noreply.github.com>
2023-10-31 20:40:56 +0800

    Items Reverted easier method

--- ItemsReverted.java
+++ ItemsReverted.java
@@ -9,9 +9,12 @@ public class ItemsReverted {
             if (array.length == 0) {
                 System.out.println("Array is empty");
             } else {
-                for (int i = array.length - 1; i >= 0; i--) {
-                    System.out.println(array[i]);
+                for (int i = 0; i < array.length; i ++){
+                    System.out.println(array[array.length - 1 -i]);
                 }
+                // for (int i = array.length - 1; i >= 0; i--) {
+                //     System.out.println(array[i]);
+                // }
             }
         }
 }
