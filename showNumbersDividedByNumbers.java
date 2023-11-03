8bac3a28c075213bd3d026cbaca834688a2601a6
TheBaytur <145964486+TheBaytur@users.noreply.github.com>
2023-11-03 21:34:29 +0800

    Show Numbers Divided By Numbers

---
+++ showNumbersDividedByNumbers.java
@@ -0,0 +1,15 @@
+public class showNumbersDividedByNumbers {
+    public static void main(String[] args) {
+        showNumbersDivededByNumbers (100, 2);
+    }
+
+    public static void showNumbersDivededByNumbers(int max, int arg){
+        for (int i = 0; i < max; i +=arg){
+            System.out.println(i);
+        }
+    }
+
+
+
+
+}
