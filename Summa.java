167bbd83e5bebac7cc5260a8c8d6e95c316598e0
TheBaytur <145964486+TheBaytur@users.noreply.github.com>
2023-11-11 17:51:44 +0800

    Logic of index sum

---
+++ Summa.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Summa {
+
+    private static int n;
+
+
+    public static void main(String[] args) {
+
+
+            Scanner scan = new Scanner(System.in);
+            System.out.format("Enter number");
+            n = scan.nextInt();
+
+
+                String one = "1";
+                long result = 0;
+                List<String> numbers = new ArrayList<>();
+                if (n<10){
+                    for (int i = 1; i < n; i++) {
+                        numbers.add(one.repeat(i));
+
+                    }
+
+                    System.out.println("Result");
+
+                    for (String num: numbers){
+                        result += Long.parseLong(num);
+                        if (numbers.indexOf(num) == numbers.size() - 1)
+                            System.out.println(num);
+                        else;
+                        System.out.printf("%s + ", num);
+                    }
+
+                    System.out.printf("\nSum: %d%n", result);
+                } else {
+                    System.out.println("Please enter number <10");
+
+                }
+
+            }
+        }
+
+
