c8388c5057a67cea850cac13e5760e0a668d5d29
TheBaytur <145964486+TheBaytur@users.noreply.github.com>
2023-12-22 14:07:02 +0800

    Bubble Sort

---
+++ BubbleSort.java
@@ -0,0 +1,22 @@
+public class BubbleSort {
+
+    public static void BubbleSort(int[] sortArr){
+        for (int i = 0; i < sortArr.length - 1; i++) {
+            for(int j = 0; j < sortArr.length - i - 1; j++) {
+                if(sortArr[j + 1] < sortArr[j]) {
+                    int swap = sortArr[j];
+                    sortArr[j] = sortArr[j + 1];
+                    sortArr[j + 1] = swap;
+                }
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        int[] sortArr = {12, 6, 4, 1, 15, 10};
+        BubbleSort(sortArr);
+        for(int i = 0; i < sortArr.length; i++){
+            System.out.print(sortArr[i] + "\n");
+        }
+    }
+}
