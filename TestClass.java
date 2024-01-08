5664bff91cf1f238cc1c1df52df8abae90bf8a0c
TheBaytur <145964486+TheBaytur@users.noreply.github.com>
2024-01-08 15:22:17 +0800

    TestClass от 0 до 1000

---
+++ TestClass.java
@@ -0,0 +1,36 @@
+public class TestClass { // выводит числа от 0 до 1000, которые делятся на 3,
+    g// но не делятся на 5, и сумма цифр в которых меньше десяти.
+
+    public static final int MAX_LIMIT = 1000;
+
+    public static void main(String[] args) {
+
+        for(int i = 0; i < MAX_LIMIT; i++) {
+            boolean enabled = true;
+            if (i % 3 != 0) {
+                enabled = false;
+            }
+
+            if (i % 5 == 0) {
+                enabled = false;
+            }
+
+            if (!testSum(i)) {
+                enabled = false;
+            }
+
+            if (enabled == true) {
+                System.out.println("Число: " + i);
+            }
+        }
+    }
+
+    public static boolean testSum(int in) {
+        int res = 0;
+        while (in > 0) {
+            res += in % 10;
+            in = in / 10;
+        }
+        return res < 10;
+    }
+}
